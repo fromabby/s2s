df5fb614ab8072c3a47550eba6e0f8a14f909920
fromabby <60252891+fromabby@users.noreply.github.com>
2022-03-13 00:53:31 +0800

    added alert provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { AuthContextProvider } from './context/authContext'
 
+import { positions, transitions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
+const options = {
+    timeout: 5000,
+    position: positions.BOTTOM_CENTER,
+    transition: transitions.SCALE
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <AlertProvider template={AlertTemplate} {...options}>
+            <AuthContextProvider>
+                <App />
+            </AuthContextProvider>
+        </AlertProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
