1d50cdfbaca6ee708e30bcfb9f325c5a03c50169
denverallam <denver.allam.iics@ust.edu.ph>
2022-03-14 22:56:49 +0800

    Hot fix for blog and comments

--- frontend/src/components/homepage/blog/comments/Comment.js
+++ frontend/src/components/homepage/blog/comments/Comment.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 
 const Comment = ({ comment }) => {
+
+    
+
     return (
         <div className="content-section-comment">
             <h1 style={{ fontStyle: "2vw" }} className={comment.user ? "content-name" : "content-name text-muted"}>
