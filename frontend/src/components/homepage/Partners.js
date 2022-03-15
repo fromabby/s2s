017fba274cbe5f3ac3360cf81a000181355f18f4
fromabby <60252891+fromabby@users.noreply.github.com>
2022-03-15 20:12:47 +0800

    added partners page content

--- frontend/src/components/homepage/Partners.js
+++ frontend/src/components/homepage/Partners.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./css/Partners.css";
 import Metadata from '../layout/Metadata'
 
 const Partners = () => {
+    const [partners, setPartners] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true)
+                const { data } = await axios.get('/api/v1/registrations')
+
+                if (data.success) {
+                    setPartners(data.registrations)
+                    setLoading(false)
+                }
+
+            } catch (error) {
+                setLoading(false)
+                setPartners([])
+            }
+        }
+        fetchData()
+    }, [])
     return (
         <div
             class="bg_image content-container"
@@ -18,19 +40,18 @@ const Partners = () => {
                         Be one of us!
                     </h1>
                     <div className="flex-container">
-                        <div className="partner">
-                            <a href="#">
-                                <img src="/images/PART.png" alt="Partnership" style={{ width: "100%" }} />
-                            </a>
-                        </div>
-                        <div className="partner">
-                            <a href="#">
-                                <img src="/images/VOL.png" alt="Volunteer" style={{ width: "100%" }} />
-                            </a>
-                        </div>
+
+                        {!loading && partners && partners.length > 0 ? partners.map(partner => (
+                            <div className="partner">
+                                <a href={partner.link}>
+                                    <img src={partner.registrationType === 1 ? "/images/PART.png" : "/images/VOL.png"} alt={partner.registrationType === 1 ? "Partnership" : "Volunteer"} style={{ width: "100%" }} />
+                                </a>
+                            </div>
+                        )) : <p>wala pa</p>}
                     </div>
                 </div>
             </div>
+
         </div>
     );
 }
