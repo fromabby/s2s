6fb79d5434a0aea5c839e385096c1356cfc4aa1d
fromabby <60252891+fromabby@users.noreply.github.com>
2022-03-15 20:07:47 +0800

    added donate page content

--- frontend/src/components/homepage/Donate.js
+++ frontend/src/components/homepage/Donate.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './css/Donate.css'
 import Metadata from '../layout/Metadata'
 
 const Donate = () => {
+    const [donationDetails, setDonationDetails] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true)
+                const { data } = await axios.get('/api/v1/donations')
+
+                if (data.success) {
+                    setDonationDetails(data.donations)
+                    setLoading(false)
+                }
+
+            } catch (error) {
+                setLoading(false)
+                setDonationDetails([])
+            }
+        }
+        fetchData()
+    }, [])
+
+    const ids = ['gcashModal', 'bpiModal']
+
     return (
         <div
             class="bg_image content-container"
@@ -11,6 +36,7 @@ const Donate = () => {
                 backgroundSize: "cover"
             }}>
             <Metadata title={`Donate`} />
+
             <div id="donate">
                 <h1 style={{ fontSize: '3.5vw' }} className="text-center">Donation Platforms</h1>
                 <div className="flex-container">
@@ -22,53 +48,29 @@ const Donate = () => {
                     </div>
                 </div>
                 {/*GCASH Modal */}
-                <div className="modal fade" id="gcashModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div className="modal-dialog modal-dialog-centered modal-fullscreen-md-down">
-                        <div className="modal-content container-fluid">
-                            <div className="modal-header">
-                                <h3 className="text-center">How to Donate (GCASH)</h3>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-                            </div>
-                            <div className="modal-body d-lg-flex flex-row">
-                                <div className="d-flex align-items-center justify-content-center mr-lg-2">
-                                    <img src="/images/gcash_logo.png" className="img-fluid" />
+                {!loading ? donationDetails.length > 0 ? donationDetails.map((donation, index) => (
+                    <div className="modal fade" id={`${ids[index]}`} aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog modal-dialog-centered modal-fullscreen-md-down">
+                            <div className="modal-content container-fluid">
+                                <div className="modal-header">
+                                    <h3 className="text-center">How to Donate ({donation.bank_name})</h3>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
                                 </div>
-                                <div className="p-lg-2 mx-lg-3">
-                                    <h5><strong>Mobile Application</strong></h5>
-                                    <p>1. Scan the Barcode Using gcash Application</p>
-                                    <p>2. Enter the Amount</p>
-                                    <p>3. Lorem ipsum dolor sit amet</p>
-                                    <p>Kindly answer the google forms link below after donating for transparency purposes:</p>
-                                    <a href="https://google.forms.com" target="_blank">https://google.forms.com</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/*BPI Modal */}
-                <div className="modal fade" id="bpiModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div className="modal-dialog modal-dialog-centered modal-fullscreen-md-down">
-                        <div className="modal-content container-fluid">
-                            <div className="modal-header">
-                                <h3 className="text-center">How to Donate (BPI)</h3>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-                            </div>
-                            <div className="modal-body d-lg-flex flex-row">
-                                <div className="d-flex align-items-center justify-content-center mr-lg-2">
-                                    <img src="/images/bpi_logo.png" className="img-fluid" />
-                                </div>
-                                <div className="p-lg-2 mx-lg-3">
-                                    <h5><strong>Mobile Application</strong></h5>
-                                    <p>1. Scan the Barcode Using gcash Application</p>
-                                    <p>2. Enter the Amount</p>
-                                    <p>3. Lorem ipsum dolor sit amet</p>
-                                    <p>Kindly answer the google forms link below after donating for transparency purposes:</p>
-                                    <a href="https://google.forms.com" target="_blank">https://google.forms.com</a>
+                                <div className="modal-body d-lg-flex flex-row">
+                                    <div className="d-flex align-items-center justify-content-center mr-lg-2">
+                                        <img src={donation.qr_code[0].path} className="img-fluid" />
+                                    </div>
+                                    <div className="p-lg-2 mx-lg-3">
+                                        <p>{donation.instructions}</p>
+                                        <p>{donation.account_details.account_name}</p>
+                                        <p>{donation.account_details.account_number}</p>
+                                        <a href={donation.donation_link} target="_blank">{donation.donation_link}</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                )) : <p>Loading...</p> : <p>walang laman</p>}
             </div>
         </div>
     )
