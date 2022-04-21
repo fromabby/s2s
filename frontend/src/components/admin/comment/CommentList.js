a1ccd07d9002efe2ea8d9ef5233e4716396c9c29
denverallam <denver.allam.iics@ust.edu.ph>
2022-04-21 11:35:04 +0800

    Changed alert message

--- frontend/src/components/admin/comment/CommentList.js
+++ frontend/src/components/admin/comment/CommentList.js
@@ -3,128 +3,129 @@ import { useAlert } from "react-alert";
 import { Button, Form } from "react-bootstrap";
 import CommentContext from "../../../context/commentContext";
 import { MDBDataTableV5 } from "mdbreact";
-import Metadata from '../../layout/Metadata'
+import Metadata from "../../layout/Metadata";
 import formatDate from "../../../formatDate";
 import Load from "../../layout/Load";
 
 const CommentList = ({ title }) => {
-    const { commentState, updateComment, adminDeleteComment } =
-        useContext(CommentContext);
-    const { allComments, isLoading } = commentState;
+  const { commentState, updateComment, adminDeleteComment } =
+    useContext(CommentContext);
+  const { allComments, isLoading } = commentState;
 
-    const alert = useAlert();
+  const alert = useAlert();
 
-    const updateItem = (comment) => {
-        updateComment(comment, !comment.status);
-        if (comment.status == 0) {
-            alert.success("Comment Allowed");
-        } else {
-            alert.error("Comment Denied");
+  const updateItem = (comment) => {
+    updateComment(comment, !comment.status);
+    if (comment.status == 0) {
+      alert.success("Comment Approved");
+    } else {
+      alert.error("Comment Denied");
+    }
+  };
+
+  const deleteItem = (id) => {
+    if (window.confirm("Are you sure you want to delete this comment?")) {
+      adminDeleteComment(id);
+      alert.success("Comment Deleted");
+    }
+  };
+
+  const setData = () => {
+    const data = {
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          width: 200,
+        },
+        {
+          label: "EMAIL",
+          field: "email",
+          width: 200,
+        },
+        {
+          label: "NAME",
+          field: "name",
+          width: 200,
+        },
+        {
+          label: "RESPONSE",
+          field: "response",
+          width: 200,
+        },
+        {
+          label: "DATE",
+          field: "date",
+          width: 200,
+        },
+        {
+          label: "STATUS",
+          field: "status",
+          width: 200,
+        },
+        {
+          label: "ACTIONS",
+          field: "actions",
+          width: 200,
+        },
+      ],
+      rows: [],
+    };
+    allComments &&
+      allComments.forEach((comment, index) => {
+        const { user, createdAt, content, status, _id: id } = comment;
+        if (status !== 1) {
+          data.rows.push({
+            id: index + 1,
+            email: user?.email,
+            name: user?.full_name,
+            response: content,
+            date: formatDate(createdAt),
+            status: status === 1 ? "Approved" : "For Approval",
+            actions: (
+              <div>
+                <Button
+                  variant="success"
+                  className="success admin-button"
+                  onClick={() => updateItem(comment, status)}
+                >
+                  Approve
+                </Button>
+                <Button
+                  variant="danger"
+                  className="danger admin-button"
+                  onClick={() => deleteItem(id)}
+                >
+                  Deny
+                </Button>
+              </div>
+            ),
+          });
         }
-    };
+      });
 
-    const deleteItem = (id) => {
-        if (window.confirm("Are you sure you want to delete this comment?")) {
-            adminDeleteComment(id);
-            alert.success("Comment Deleted");
-        }
-    };
+    return data;
+  };
 
-    const setData = () => {
-        const data = {
-            columns: [
-                {
-                    label: "ID",
-                    field: "id",
-                    width: 200,
-                },
-                {
-                    label: "EMAIL",
-                    field: "email",
-                    width: 200,
-                },
-                {
-                    label: "NAME",
-                    field: "name",
-                    width: 200,
-                },
-                {
-                    label: "RESPONSE",
-                    field: "response",
-                    width: 200,
-                },
-                {
-                    label: "DATE",
-                    field: "date",
-                    width: 200,
-                },
-                {
-                    label: "STATUS",
-                    field: "status",
-                    width: 200,
-                },
-                {
-                    label: "ACTIONS",
-                    field: "actions",
-                    width: 200,
-                },
-            ],
-            rows: [],
-        };
-        allComments &&
-            allComments.forEach((comment, index) => {
-                const { user, createdAt, content, status, _id: id } = comment;
-                data.rows.push({
-                    id: index + 1,
-                    email: user?.email,
-                    name: user?.full_name,
-                    response: content,
-                    date: formatDate(createdAt),
-                    status: status === 1 ? "Allowed" : "Denied",
-                    actions: (
-                        <div>
-                            <Button
-                                variant={status === 1 ? "danger" : "success"}
-                                className={`${status === 1 ? "danger" : "success"
-                                    } admin-button`}
-                                onClick={() => updateItem(comment, status)}
-                            >
-                                {status === 1 ? "Deny" : "Allow"}
-                            </Button>
-                            <Button
-                                variant="danger"
-                                className="danger admin-button"
-                                onClick={() => deleteItem(id)}
-                            >
-                                Delete
-              </Button>
-                        </div>
-                    ),
-                });
-            });
-
-        return data;
-    };
-
-    return (
-        <>
-            <Metadata title={title} />
-            {isLoading ? (
-                <Load />
-            ) : (
-                <MDBDataTableV5
-                    hover
-                    entriesOptions={[5, 20, 25]}
-                    entries={5}
-                    pagesAmount={4}
-                    data={setData()}
-                    fullPagination
-                    searchTop
-                    searchBottom={false}
-                />
-            )}
-        </>
-    )
+  return (
+    <>
+      <Metadata title={title} />
+      {isLoading ? (
+        <Load />
+      ) : (
+        <MDBDataTableV5
+          hover
+          entriesOptions={[5, 20, 25]}
+          entries={5}
+          pagesAmount={4}
+          data={setData()}
+          fullPagination
+          searchTop
+          searchBottom={false}
+        />
+      )}
+    </>
+  );
 };
 
 export default CommentList;
